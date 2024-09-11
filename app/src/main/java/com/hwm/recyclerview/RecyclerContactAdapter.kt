3ad9cdd8dd6fdb257490e1ddb955d8675bbc28e8
Haroon Waheed <haroonwaheed002@gmail.com>
2024-09-11 22:10:14 +0500

    Added the functionality to update and delete the RecyclerView content on RunTime

--- app/src/main/java/com/hwm/recyclerview/RecyclerContactAdapter.kt
+++ app/src/main/java/com/hwm/recyclerview/RecyclerContactAdapter.kt
@@ -1,10 +1,16 @@
 package com.hwm.recyclerview
 
+import android.app.Dialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerContactAdapter(var contactList: MutableList<contactModal>) : RecyclerView.Adapter<RecyclerContactAdapter.ViewHolder>() {
@@ -22,6 +28,66 @@ class RecyclerContactAdapter(var contactList: MutableList<contactModal>) : Recyc
             imgContact.setImageResource(contact.img)
             txtName.text = contact.name
             txtNumber.text = contact.number
+            llView.setOnClickListener {
+                // Handle click on the contact item
+
+                val dialog = Dialog(llView.context, R.style.CustomDialog)
+                // Inflate the dialog layout
+                val dialogView = LayoutInflater.from(llView.context).inflate(R.layout.add_update_layout, null)
+                dialog.setContentView(dialogView)
+                val etName = dialogView.findViewById<EditText>(R.id.etName)
+                val etNumber = dialogView.findViewById<EditText>(R.id.etNumber)
+                val etAction = dialogView.findViewById<Button>(R.id.btnAction)
+                val textView = dialogView.findViewById<TextView>(R.id.tvTitle)
+
+                textView.text = "Update Contact"
+                etAction.text = "Update"
+
+                etName.setText(contact.name)
+                etNumber.setText(contact.number)
+
+                // Set the click listener for the "Update" button in the dialog
+                etAction.setOnClickListener {
+                    val name = etName.text.toString().trim()
+                    val number = etNumber.text.toString().trim()
+
+                    if (name.isEmpty() && number.isEmpty()) {
+                        Toast.makeText(holder.itemView.context, "Name and Number are Empty", Toast.LENGTH_LONG).show()
+                    } else if (name.isEmpty()) {
+                        Toast.makeText(holder.itemView.context, "Name is Empty", Toast.LENGTH_LONG).show()
+                    } else if (number.isEmpty()) {
+                        Toast.makeText(holder.itemView.context, "Number is Empty", Toast.LENGTH_LONG).show()
+                    } else {
+                        contactList[position].name = name
+                        contactList[position].number = number
+                        notifyItemChanged(position)
+                        dialog.dismiss()
+                    }
+                }
+                // Show the dialog to the user
+                dialog.show()
+            }
+
+            llView.setOnLongClickListener{
+                // Handle long click on the contact item
+
+                //setting alert Dialog Button
+                var builder  = AlertDialog.Builder(llView.context)
+                builder.setTitle("Delete Contact")
+                builder.setMessage("Are you sure you want to delete this contact?")
+                builder.setIcon(R.drawable.baseline_delete_24)
+                builder.setPositiveButton("Yes"){
+                    dialog,which->
+                    contactList.removeAt(position)
+                    notifyItemRemoved(position)
+                }
+                builder.setNegativeButton("No"){
+                    dialog,which->
+                }
+
+                builder.show()
+                true
+            }
         }
     }
 
@@ -35,5 +101,6 @@ class RecyclerContactAdapter(var contactList: MutableList<contactModal>) : Recyc
         val imgContact: ImageView = itemView.findViewById(R.id.imContact)
         val txtName: TextView = itemView.findViewById(R.id.Contact)
         val txtNumber: TextView = itemView.findViewById(R.id.ContactNumber)
+        val llView: LinearLayout = itemView.findViewById(R.id.llView)
     }
 }
--- app/src/main/java/com/hwm/recyclerview/contactModal.kt
+++ app/src/main/java/com/hwm/recyclerview/contactModal.kt
@@ -2,6 +2,6 @@ package com.hwm.recyclerview
 
 data class contactModal(
     val img: Int,
-    val name: String,
-    val number: String
+    var name: String,
+    var number: String
 )
